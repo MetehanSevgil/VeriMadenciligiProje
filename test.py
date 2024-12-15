71856bedf5b46f13ed283e5e33d9fa61217659ef
MetehanSevgil <030122059@std.izu.edu.tr>
2024-12-15 14:50:01 +0300

    test.py model tahmin oranlarını gösterecek şekilde güncellendi

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ import numpy as np
 
 # Model ve test görseli yolu
 MODEL_PATH = "C:\\Users\\Metehan Sevgil\\Desktop\\VeriMadenciligiProje\\deneme_resnet50_model.keras"  # Model yolu
-TEST_IMAGE_PATH = "C:\\Users\\Metehan Sevgil\\Desktop\\VeriMadenciligiProje\\Veriler\\brokoli\\brokoli1_aug4.jpg"  # Test görüntüsü yolu
+TEST_IMAGE_PATH = "C:\\Users\\Metehan Sevgil\\Desktop\\VeriMadenciligiProje\\Veriler\\salata\\salata1.jpg"  # Test görüntüsü yolu
 CLASS_NAMES = ['acili_ezme',
                 'ayran_corbasi',
                 'baklava',
@@ -39,13 +39,22 @@ IMG_SIZE = (256, 256)
 model = load_model(MODEL_PATH)
 print("Model yüklendi!")
 
-# Görüntüyü hazırla
-img = tf.image.decode_jpeg(tf.io.read_file(TEST_IMAGE_PATH), channels=3)  # Görüntüyü yükle ve aç
-img = tf.image.resize(img, IMG_SIZE)  # Görüntüyü boyutlandır
-img_array = tf.expand_dims(img, axis=0)  # Batch boyutunu ekle
+# Test görüntüsünü yükle
+img = tf.keras.preprocessing.image.load_img(TEST_IMAGE_PATH, target_size=IMG_SIZE)
+img_array = tf.keras.preprocessing.image.img_to_array(img)
+img_array = tf.expand_dims(img_array, 0)  # Resmi modele uygun şekle getir
 img_array = img_array / 255.0  # Normalizasyon
 
 # Tahmin yap
 predictions = model.predict(img_array)
-predicted_class = np.argmax(predictions[0])  # En yüksek olasılıklı sınıf
-print(f"Tahmin Edilen Sınıf: {CLASS_NAMES[predicted_class]}")
+score = tf.nn.softmax(predictions[0])
+
+# Sonuçları yazdır
+print("Bu görüntü en olası şekilde {} sınıfına ait.".format(CLASS_NAMES[np.argmax(score)]))
+print("Tahmin olasılıkları:")
+
+for i in range(len(CLASS_NAMES)):
+    print("{}: {:.2f}%".format(CLASS_NAMES[i], 100 * score[i]))
+
+# Test görüntüsünü göster
+img.show()
