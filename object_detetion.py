7984e127b7ac08dd26b8d7cb1e46467798bf6ece
MetehanSevgil <030122059@std.izu.edu.tr>
2024-12-21 21:10:34 +0300

    en son eğitilen güncel model oluşturuldu

--- object_detetion.py
+++ object_detetion.py
@@ -1,10 +1,7 @@
 from ultralytics import YOLO
 
-# Modeli yükle
 model = YOLO("yolov8n.pt")
 
-# Modeli eğitin
-results = model.train(data="data.yaml", epochs=20, batch_size=64)
+results = model.train(data="data.yaml", epochs=50, batch=32)
 
-# Modeli kaydet
-model.save("yolov8n_trained.pt")
+model.save("yolov8n_trained_ensonmodel.pt")
--- yolo_test.py
+++ yolo_test.py
@@ -1,7 +1,5 @@
 from ultralytics import YOLO
 
-# Eğitilen modeli yükle
-model = YOLO("yolov8n_trained.pt")
+model = YOLO("yolov8n_trained_ensonmodel.pt")
 
-# Test görüntüsü üzerinde tahmin yap
-results = model(source="test_image.jpg", save=True, conf=0.3)
+results = model(source="test_image.jpeg", save=True, conf=0.2)
