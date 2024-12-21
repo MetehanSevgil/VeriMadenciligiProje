96c3dfb482ce2be699ee02b30e2affc67d6e5d1e
Yusuf Çokal <134581731+yusuffcx@users.noreply.github.com>
2024-12-21 15:33:48 +0300

    yemekler.json olusturuldu , front-end için tablo eklendi

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,5 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:frontend/Page1.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+final theme = ThemeData(
+  useMaterial3: true,
+  colorScheme:
+      ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 218, 250, 40)),
+  textTheme: GoogleFonts.latoTextTheme(),
+);
 
 void main() {
   runApp(const MyApp());
@@ -11,14 +19,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Veri  Madenciligi Proje',
-      theme: ThemeData(
-
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
+      title: 'Veri Madenciligi Proje',
+      theme: theme,
       home: Page1Init(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
-
